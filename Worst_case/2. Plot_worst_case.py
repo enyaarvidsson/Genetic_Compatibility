5aae7aae3c034934c71f0a702f33610342b277b2
Johanna Lundstrom <jolunds@saga.math.chalmers.se>
2025-03-25 17:42:44 +0100

    Gjort om med filtrerad blast results

--- Worst_case/2. Plot_worst_case.py
+++ Worst_case/2. Plot_worst_case.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-gene_name = "ErmB"
+gene_name = "NDM-1"
 
 file_path = f"/storage/jolunds/REVISED/WORST_CASE/worst_case_split/worst_case_{gene_name}.csv"
 worst_case_df = pd.read_csv(file_path, sep=",").T.reset_index()
-worst_case_df.rename(columns={'index': 'Bacteria_ID'})
+worst_case_df.rename(columns={'index': 'Bacteria_ID', 0: 'Worst_case'}, inplace=True)
 
 # Add phylum column
 path = "/storage/shared/data_for_master_students/enya_and_johanna/genome_full_lineage.tsv"
@@ -24,33 +24,34 @@ top_phyla = worst_case_df["Phylum"].value_counts().head(6)
 worst_case_df = worst_case_df[worst_case_df['Phylum'].isin(top_phyla.index)]
     
 # Add match status column
-path = f"/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/taxonomy_results_{gene_name}.csv" # this contains matches
+path = f"/storage/jolunds/REVISED/TAXONOMY/new_taxonomy_split_genes/taxonomy_results_{gene_name}.csv" # this contains matches
     
 taxonomy_df = pd.read_csv(path, sep=",") 
+
    
 matching_df = taxonomy_df[['Bacteria_ID']].drop_duplicates()
 worst_case_df = worst_case_df.merge(matching_df.assign(Match_status="Match"), on="Bacteria_ID", how="left").fillna("No_match") # here a new column is added to euclidean_gene_df called "Match_status" and it says if there are Match
 
 # Ta bort hälften av no_match (no_match: 94302)
-no_match_count = (worst_case_df['Match_status'] == 'no_match').sum()
+no_match_count = (worst_case_df['Match_status'] == 'No_match').sum()
+match_count = (worst_case_df['Match_status'] == 'Match').sum()
+print(match_count)
+
+
 no_match_indices = worst_case_df[worst_case_df['Match_status'] == 'No_match'].index
 
-num_to_remove = no_match_count**0.8
+num_to_remove = no_match_count - 2*match_count
 random_indices = np.random.choice(no_match_indices, num_to_remove, replace=False)
 
 worst_case_df_filtered= worst_case_df.drop(random_indices).reset_index(drop=True)
 
-print(len(worst_case_df_filtered))
 #worst_case_df_filtered = worst_case_df[:20_000]
-
-sns.histplot(worst_case_df, x="Worst_case", hue="Match_status", hue_order=["No_match", "Match"],
-             multiple="stack", bins=30)
-plt.savefig("/home/jolunds/newtest/worst_case.png")
-plt.close()
 
 x_min = worst_case_df_filtered["Worst_case"].min()
 x_max = worst_case_df_filtered["Worst_case"].max()
-num_bins = 15  # Set the desired number of bins
+
+
+num_bins = 30  # Set the desired number of bins
 bin_edges = np.linspace(x_min, x_max, num_bins + 1) 
 # Plot histogram
 g = sns.FacetGrid(worst_case_df_filtered, col="Phylum",  col_order = top_phyla.index, sharey=False,
@@ -60,7 +61,7 @@ g.map_dataframe(sns.histplot, x="Worst_case", hue="Match_status", hue_order=["No
 g.set_axis_labels("Worst Case", "Number of Bacteria")
 g.set_titles(col_template="{col_name}")
 
-plt.savefig(f"/home/jolunds/newtest/worst_case{gene_name}.png")
+plt.savefig(f"/home/jolunds/newtest/worst_case_plots/worst_case{gene_name}.png")
 plt.close()
 
 

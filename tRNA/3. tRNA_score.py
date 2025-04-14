d94ad6550a8786c0a932dfd12744c2a9ce2d7d1b
Enya Arvidsson <enyaa@saga.math.chalmers.se>
2025-04-14 14:06:25 +0200

    tRNA

--- tRNA/3. tRNA_score.py
+++ tRNA/3. tRNA_score.py
@@ -3,180 +3,110 @@ import re
 from collections import Counter
 from Bio.Seq import Seq
 
-def tRNA_score(codon, codon_value, codon_dist):
-    if codon in codon_dist:
-        difference = max(0, codon_value-codon_dist[codon])
-        return 0.1*difference
+def tRNA_score(gene_dist, genome_dist):
+    #codons =[] #kanske behövs vid dubbelsidigt
+    codon_scores = []
+    for gene_codon, gene_value in gene_dist.items(): 
+        if gene_codon in genome_dist:
+            difference = max(0, gene_value-genome_dist[gene_codon])
+            codon_scores.append(0.1*difference)
+        else:
+            first_two = gene_codon[:2]
+            third = gene_codon[2]
+
+            # Check wobble position 
+            matched = False
+            for genome_codon, genome_value in genome_dist.items():
+                if genome_codon[:2] == first_two:
+                    if third == "T" and genome_codon[2] == "C":
+                        difference = max(0, gene_value - genome_value)
+                        codon_scores.append(0.2*difference)
+                        matched = True
+                        break
+                    elif third == "G" and genome_codon[2] == "A":
+                        difference = max(0, gene_value - genome_value)
+                        codon_scores.append(0.2*difference)
+                        matched = True
+                        break
+                    elif genome_codon[2] == "T" and third != "G":
+                        difference = max(0, gene_value - genome_value)
+                        codon_scores.append(0.9*difference)
+                        matched = True
+                        break
+            if not matched:
+                difference = gene_value
+                codon_scores.append(difference)
+
+    return sum(codon_scores)
+
+# Load file with gene_names (sorted)
+gene_name_file = "/storage/enyaa/REVISED/gene_names.txt"
+gene_names_df = pd.read_csv(gene_name_file, header=None, names=["Gene_name"])
+
+gene_codons_file = '/storage/enyaa/REVISED/tRNA/codons_genes.csv'
+gene_codons_df = pd.read_csv(gene_codons_file)
+
+file = "/storage/enyaa/REVISED/KMER/euclidean_split_genes_filtered/euclidean_df_tet(Q).pkl"
+filtered_df = pd.read_pickle(file)
+bacteria_ids = filtered_df["Bacteria_ID"].tolist()
+
+path = "/storage/shared/data_for_master_students/enya_and_johanna/genome_full_lineage.tsv"
+full_lineage_df = pd.read_csv(path, sep="\t", header=None)
+full_lineage_df.columns = ["Bacteria_ID", "Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+phylum_mapping = full_lineage_df[["Bacteria_ID", "Phylum", "Species"]] # only the bacteria_id and the respective phylum & species
+
+
+
+for gene_name in gene_names_df["Gene_name"][:2]:
+    gene_df = gene_codons_df[gene_codons_df['Gene_name'] == gene_name].reset_index(drop=True)
+    stop_codons = ["TAA", "TAG", "TGA"]
+
+    # Remove rows where 'Codon' is a stop codon
+    gene_df = gene_df[~gene_df['Codon'].isin(stop_codons)].reset_index(drop=True)
+    gene_df["Frequency"] = (
+            gene_df["Count"] / gene_df["Count"].sum()
+        )
     
-    first_two = codon[:2]
-    third = codon[2]
-
-     # Check 0.5 rules first
-    for dist_codon, dist_value in codon_dist.items():
-        if dist_codon[:2] == first_two:
-            if third == "T" and dist_codon[2] == "C":
-                difference = max(0.05, codon_value - dist_value)
-                return 0.2*difference
-            elif third == "G" and dist_codon[2] == "A":
-                difference = max(0.05, codon_value - dist_value)
-                return 0.2*difference
-
-    # Check 0.1 rule
-    for dist_codon, dist_value in codon_dist.items():
-        if dist_codon[:2] == first_two and dist_codon[2] == "T" and third != "G":
-            difference = max(0.05, codon_value - dist_value)
-            return 0.9*difference
-
-    return codon_value
-
-
-def count_codons(sequence):
-    # Remove everything that isn't a base
-    sequence = re.sub(r'[^ATGC]', '', sequence.upper()) 
-    # Find codons
-    codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
+    # Convert to a dictionary with Codon as the key and Count as the value
+    gene_dict = gene_df.set_index("Codon")["Frequency"].to_dict()
     
-    # Count codons
-    codons_count = Counter(codons)
-    
-    codon_df = pd.DataFrame(codons_count.items(), columns=["Codon", "Count"])
+    tRNA_score_list = []
+    for bacteria_id in bacteria_ids[:1]:
+        tRNA_file = f"/storage/jolunds/REVISED/tRNA/{bacteria_id}_trnascan.txt"
+        tRNA_df = pd.read_csv(tRNA_file, sep="\t", comment="#", skiprows=3, header=None)
+        tRNA_df.columns = ["Sequence_name", "tRNA_nr", "Begin", "End", "tRNA_type", "Anticodon", "Intron_begin", "Intron_end", "Score", "Comment"]
+        
+        genome_df = tRNA_df["Anticodon"].value_counts().reset_index()
+        genome_df.columns = ["Anticodon", "Count"]
+        
+        # Reverse complement 
+        genome_df["Codon"] = genome_df["Anticodon"].apply(
+            lambda anticodon: str(Seq(anticodon).reverse_complement())
+        )
+        
+        # Distribution
+        genome_df["Frequency"] = (
+            genome_df["Count"] / genome_df["Count"].sum()
+        )
+        
+        # Change to dictionary - Codon as the key and Count as the value
+        genome_dict = genome_df.set_index("Codon")["Frequency"].to_dict() 
 
-    # Return the counts of anticodons
-    return codon_df
+        # tRNA score
+        tRNA_value = tRNA_score(gene_dict, genome_dict)
+        
+        # Append results
+        tRNA_score_list.append({"Bacteria_ID": bacteria_id, "tRNA_score": tRNA_value})
 
-# E.coli -------------------
-Ecoli_file = "/home/jolunds/newtest/ecoli_trnascan_results.txt"
-Ecoli_tRNA_df = pd.read_csv(Ecoli_file, sep="\t", comment="#", skiprows=3, header=None)
-Ecoli_tRNA_df.columns = ["Sequence_name", "tRNA_nr", "Begin", "End", "tRNA_type", "Anticodon", "Intron_begin", "Intron_end", "Score", "Comment"]
-Ecoli_anticodon_counts_df = Ecoli_tRNA_df["Anticodon"].value_counts().reset_index()
-Ecoli_anticodon_counts_df.columns = ["Anticodon", "Ecoli_count"] 
+    gene_tRNA_df = pd.DataFrame(tRNA_score_list)
+    gene_tRNA_df = gene_tRNA_df.merge(phylum_mapping, on='Bacteria_ID', how='left')
 
-# Reverse complement anticodons to get codons
-Ecoli_anticodon_counts_df["Codon"] = Ecoli_anticodon_counts_df["Anticodon"].apply(
-    lambda anticodon: str(Seq(anticodon).reverse_complement())
-)
-Ecoli_anticodon_counts_df["Frequency"] = (
-    Ecoli_anticodon_counts_df["Ecoli_count"] / Ecoli_anticodon_counts_df["Ecoli_count"].sum()
-)
-# Change tp dictionary
-Ecoli_codon_dist = dict(zip(Ecoli_anticodon_counts_df["Codon"], Ecoli_anticodon_counts_df["Frequency"]))
+    # Save
+    if "/" in gene_name:
+        gene_name = gene_name.rename("/", "?")
+        
+    save_path = f"/storage/enyaa/REVISED/tRNA/tRNA_score/tRNA_score_{gene_name}.csv"
+    gene_tRNA_df.to_csv(save_path, index=False)
 
-# B.subtilis ------------------
-Bsubtilis_file = "/storage/enyaa/trna_scan/b_subtilis_trnascan_results.txt"
-Bsubtilis_tRNA_df = pd.read_csv(Bsubtilis_file, sep="\t", comment="#", skiprows=3, header=None)
-Bsubtilis_tRNA_df.columns = ["Sequence_name", "tRNA_nr", "Begin", "End", "tRNA_type", "Anticodon", "Intron_begin", "Intron_end", "Score", "Comment"]
-Bsubtilis_anticodon_counts_df = Bsubtilis_tRNA_df["Anticodon"].value_counts().reset_index()
-Bsubtilis_anticodon_counts_df.columns = ["Anticodon", "Bsubtilis_count"] 
 
-# Reverse complement anticodons to get codons
-Bsubtilis_anticodon_counts_df["Codon"] = Bsubtilis_anticodon_counts_df["Anticodon"].apply(
-    lambda anticodon: str(Seq(anticodon).reverse_complement())
-)
-Bsubtilis_anticodon_counts_df["Frequency"] = (
-    Bsubtilis_anticodon_counts_df["Bsubtilis_count"] / Bsubtilis_anticodon_counts_df["Bsubtilis_count"].sum()
-)
-# Change tp dictionary
-Bsubtilis_codon_dist = dict(zip(Bsubtilis_anticodon_counts_df["Codon"], Bsubtilis_anticodon_counts_df["Frequency"]))
-
-# B.fragilis --------------------
-Bfragilis_file = "/storage/enyaa/trna_scan/B_fragilis_trnascan_results.txt"
-Bfragilis_tRNA_df = pd.read_csv(Bfragilis_file, sep="\t", comment="#", skiprows=3, header=None)
-Bfragilis_tRNA_df.columns = ["Sequence_name", "tRNA_nr", "Begin", "End", "tRNA_type", "Anticodon", "Intron_begin", "Intron_end", "Score", "Comment"]
-Bfragilis_anticodon_counts_df = Bfragilis_tRNA_df["Anticodon"].value_counts().reset_index()
-Bfragilis_anticodon_counts_df.columns = ["Anticodon", "Bfragilis_count"] 
-
-# Reverse complement anticodons to get codons
-Bfragilis_anticodon_counts_df["Codon"] = Bfragilis_anticodon_counts_df["Anticodon"].apply(
-    lambda anticodon: str(Seq(anticodon).reverse_complement())
-)
-Bfragilis_anticodon_counts_df["Frequency"] = (
-    Bfragilis_anticodon_counts_df["Bfragilis_count"] / Bfragilis_anticodon_counts_df["Bfragilis_count"].sum()
-)
-# Change tp dictionary
-Bfragilis_codon_dist = dict(zip(Bfragilis_anticodon_counts_df["Codon"], Bfragilis_anticodon_counts_df["Frequency"]))
-
-# NDM-6 -------------------------------
-NDM6_seqeunce = """ATGGAATTGCCCAATATTATGCACCCGGTCGCGAAGCTGAGCACCGCATTAGCCGCTGCA
-TTGATGCTGAGCGGGTGCATGCCCGGTGAAATCCGCCCGACGATTGGCCAGCAAATGGAA
-ACTGGCGACCAACGGTTTGGCGATCTGGTTTTCCGCCAGCTCGCACCGAATGTCTGGCAG
-CACACTTCCTATCTCGACATGCCGGGTTTCGGGGCAGTCGCTTCCAACGGTTTGATCGTC
-AGGGATGGCGGCCGCGTGCTGGTGGTCGATACCGCCTGGACCGATGACCAGACCGCCCAG
-ATCCTCAACTGGATCAAGCAGGAGATCAACCTGCCGGTCGCGCTGGCGGTGGTGACTCAC
-GCGCATCAGGACAAGATGGGCGGTATGGACGCGCTGCATGCGGCGGGGATTGCGACTTAT
-GCCAATGCGTTGTCGAACCAGCTTGCCCCGCAAGAGGGGATGGTTGCGGCGCAACACAGC
-CTGACTTTCGCCGCCAATGGCTGGGTCGAACCAGCAACCGCGCCCAACTTTGGCCCGCTC
-AAGGTATTTTACCCCGGCCCCGGCCACACCAGTGACAATATCACCGTTGGGATCGACGGC
-ACCGACATCGCTTTTGGTGGCTGCCTGATCAAGGACAGCAAGGCCAAGTCGCTCGGCAAT
-CTCGGTGATGCCGACACTGAGCACTACGCCGCGTCAGTGCGCGCGTTTGGTGCGGCGTTC
-CCCAAGGCCAGCATGATCGTGATGAGCCATTCCGCCCCCGATAGCCGCGCCGCAATCACT
-CATACGGCCCGCATGGCCGACAAGCTGCGCTGA"""
-
-NDM6_codons = count_codons(NDM6_seqeunce)
-NDM6_codons["Frequency"] = NDM6_codons["Count"] / NDM6_codons["Count"].sum()
-
-NDM6_scores = []
-for _, row in NDM6_codons.iterrows():
-    codon = row["Codon"]
-    freq = row["Frequency"]
-    ecoli_codon_score = tRNA_score(codon, freq, Ecoli_codon_dist)
-    bsubtilis_codon_score = tRNA_score(codon, freq, Bsubtilis_codon_dist)
-    bfragilis_codon_score = tRNA_score(codon, freq, Bfragilis_codon_dist)
-    NDM6_scores.append((
-        codon,
-        ecoli_codon_score,
-        bsubtilis_codon_score,
-        bfragilis_codon_score
-    ))
-
-NDM6_score_df = pd.DataFrame(NDM6_scores, columns=["Codon", "Ecoli_score", "Bsubtilis_score", "Bfragilis_score"])
-
-NDM6_total_scores = NDM6_score_df[["Ecoli_score", "Bsubtilis_score", "Bfragilis_score"]].sum()
-print(NDM6_total_scores)
-
-# tet(Q) -------------------
-tetQ_seq = """GTGCGTTTCGACAATGCATCTATTGTAGTATATTATTGCTTAATCCAAATGAATATTATAAATTTAGGAATTCTTGCTCACATTGATGCA
-GGAAAAACTTCCGTAACCGAGAATCTGCTGTTTGCCAGTGGAGCAACGGAAAAGTGCGGCCGTGTGGATAATGGTGACACCATAACAGAC
-TCTATGGATATAGAGAAACGTAGAGGAATTACTGTTCGGGCTTCTACGACATCTATTATCTGGAATGGAGTGAAATGCAATATCATTGAC
-ACTCCGGGACACATGGATTTTATTGCGGAAGTGGAGCGGACATTCAAAATGCTTGATGGAGCAGTCCTCATCTTATCCGCAAAGGAAGGC
-ATACAAGCGCAAACAAAGTTGCTGTTCAATACTTTACAAAAACTGCAAATCCCGACAATTATATTTATCAATAAAATTGACCGTGACGGT
-GTGAATTTAGAGCGTTTGTATCTGGATATAAAAACAAATCTGTCTCAAGATGTCCTGTTTATGCAAACTGTTGTCGATGGATTGGTTTAT
-CCGATTTGCTCCCAAACATATATAAAGGAAGAATACAAAGAATTTGTATGCAACCATGACGACAATATATTAGAACGATATTTGGCGGAT
-AGCGAAATTTCACCGGCTGATTATTGGAATACGATAATCGATCTTGTGGCAAAAGCCAAAGTCTATCCGGTACTACATGGATCAGCAATG
-TTCAATATCGGTATCAATGAGTTGTTGGACGCCATCTCTTCTTTTATACTTCCTCCAGAATCAGTCTCAAACAGACTTTCAGCTTATCTC
-TATAAGATAGAGCATGACCCCAAAGGACATAAAAGAAGTTTTCTAAAAATAATTGACGGAAGTCTGAGACTTCGAGACATTGTAAGAATC
-AACGATTCGGAAAAATTCATCAAGATTAAAAATCTAAAGACTATTTATCAGGGCAGAGAGATAAATGTTGATGAAGTGGGGGCCAATGAT
-ATCGCGATTGTAGAAGATATGGAAGATTTTCGAATCGGAGATTATTTAGGTACTAAACCTTGTTTGATTCAAGGGTTATCTCATCAGCAT
-CCCGCTCTCAAATCCTCCGTCCGGCCAGACAGGTCCGAAGAGAGAAGCAAGGTGATATCCGCTCTGAATACATTGTGGATTGAAGACCCG
-TCTTTGTCCTTTTCCATAAACTCATATAGTGATGAATTGGAAATCTCGTTATATGGTTTGACACAAAAGGAAATCATACAGACATTGCTG
-GAAGAACGATTTTCCGTAAAGGTCCATTTTGATGAGATCAAGACTATCTACAAAGAACGACCTGTAAAAAAGGTCAATAAGATTATTCAG
-ATCGAAGTGCCACCCAACCCTTACTGGGCCACAATAGGGCTGACGCTTGAACCCTTGCCGTTAGGGACAGGGTTGCAAATCGAAAGTGAC
-ATCTCCTATGGTTATCTGAACCATTCTTTTCAAAATGCCGTTTTTGAAGGGATTCGTATGTCTTGCCAATCTGGTTTACATGGATGGGAA
-GTGACTGATCTGAAAGTAACTTTTACTCAAGCCGAGTATTATAGCCCGGTAAGTACACCTGCTGATTTCAGACAGCTGACCCCTTATGTC
-TTCAGGCTGGCCTTGCAACAGTCAGGTGTGGACATTCTCGAACCGATGCTCTATTTTGAGTTGCAGATACCCCAAGCGGCAAGTTCCAAA
-GCTATTACAGATTTGCAAAAAATGATGTCTGAGATTGAAGACATCAGTTGCAATAATGAGTGGTGTCATATTAAAGGGAAAGTTCCATTA
-AATACAAGTAAAGACTACGCCTCAGAAGTAAGTTCATACACTAAGGGCTTAGGCGTTTTTATGGTCAAGCCATGCGGGTATCAAATAACA
-AAAGGCGATTATTCTGATAATATCCGCATGAACGAAAAAGATAAACTTTTATTCATGTTCCAAAAATCAATGTCATCAAAATAA"""
-
-tetQ_codons = count_codons(tetQ_seq)
-tetQ_codons['Frequency'] = tetQ_codons['Count'] / tetQ_codons["Count"].sum()
-tetQ_scores = []
-for _, row in tetQ_codons.iterrows():
-    codon = row['Codon']
-    freq = row['Frequency']
-    ecoli_codon_score = tRNA_score(codon, freq, Ecoli_codon_dist)
-    bsubtilis_codon_score = tRNA_score(codon, freq, Bsubtilis_codon_dist)
-    bfragilis_codon_score = tRNA_score(codon, freq, Bfragilis_codon_dist)
-    
-    tetQ_scores.append((
-        codon,
-        ecoli_codon_score,
-        bsubtilis_codon_score,
-        bfragilis_codon_score
-    ))
-
-tetQ_score_df = pd.DataFrame(tetQ_scores, columns=["Codon", "Ecoli_score", "Bsubtilis_score", "Bfragilis_score"])
-tetQ_total_scores = tetQ_score_df[["Ecoli_score", "Bsubtilis_score", "Bfragilis_score"]].sum()
-print(tetQ_total_scores)
-
-#score_tetQ = tetQ_score_df['Score'].sum()/len(tetQ_score_df)
-#print(score_tetQ)
+        
---
+++ tRNA/4. tRNA_histogram.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import numpy as np
+
+gene_name = "tet(Q)"
+
+if "/" in gene_name:
+    gene_name = gene_name.rename("/", "?")
+    
+file_path = f"/storage/enyaa/REVISED/tRNA/tRNA_score/tRNA_score_{gene_name}.csv"
+tRNA_score_df = pd.read_csv(file_path)
+
+# Load taxonomy results and count matches
+taxonomy_file = f"/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/taxonomy_results_{gene_name}.csv"
+if os.path.exists(taxonomy_file):
+    taxonomy_df = pd.read_csv(taxonomy_file)
+    taxonomy_df.columns = ["Gene_name", "Bacteria_ID", "Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+    taxonomy_df = taxonomy_df.drop_duplicates() # Remove duplicates (when we have multiple matches in one genome)
+    matching_df = taxonomy_df[['Bacteria_ID']]
+    tRNA_score_df = tRNA_score_df.merge(matching_df.assign(Match_status="Match"), on="Bacteria_ID", how="left").fillna("No_match") # here a new column is added to euclidean_gene_df called "Match_status" and it says if there are Match
+    matches = 1
+else: # If taxonomy file do not exist
+    tRNA_score_df["Match_status"] = "No_match"
+    matches = 0
+    
+# Filter for top phyla
+top_phyla = tRNA_score_df["Phylum"].value_counts().head(6)
+tRNA_score_df = tRNA_score_df[tRNA_score_df['Phylum'].isin(top_phyla.index)]
+
+nr_bins = 30
+min_value = tRNA_score_df["tRNA_score"].min()
+max_value = tRNA_score_df["tRNA_score"].max() + 0.001 # so all values fall inside the max_value
+bin_edges = np.linspace(min_value, max_value, nr_bins + 1)
+
+# DOWNSAMPLE
+downsampled_no_matches = [] # will become a list of dataframes
+tRNA_score_df = tRNA_score_df.copy()
+tRNA_score_df["Phylum"] = tRNA_score_df["Phylum"].astype(str)
+
+for phylum, phylum_df in tRNA_score_df.groupby("Phylum"): # phylum - name of phylum, phylum_df - df that has only rows from that phylum
+    match_count = (phylum_df["Match_status"] == "Match").sum()
+    # create a df for match and a df for no_match
+    matches_phylum_df = phylum_df[phylum_df["Match_status"] == "Match"]
+    no_matches_phylum_df = phylum_df[phylum_df["Match_status"] == "No_match"]  
+
+    # How many no_matches to keep
+    if matches == 1: # if matches exists
+        if match_count == 0:
+            keep_size = 10000
+        elif match_count < 100:
+            keep_size = 2000
+        elif match_count < 3000:
+            keep_size = 10000
+        else:
+            keep_size = match_count * 3
+    else:
+        keep_size = len(phylum_df) 
+
+    # Downsample no_matches
+    if keep_size > len(no_matches_phylum_df): # make sure np.random.choice works, so the keep_size is not bigger than the population size
+        keep_size = len(no_matches_phylum_df)
+    downsampled_no_matches_phylum_df = no_matches_phylum_df.iloc[np.random.choice(len(no_matches_phylum_df), keep_size, replace=False)]
+
+    # Append both "Match" and downsampled "No_match" bacteria
+    downsampled_no_matches.append(pd.concat([matches_phylum_df, downsampled_no_matches_phylum_df]))
+
+# Combine all phyla into the final df
+tRNA_downsampled_df = pd.concat(downsampled_no_matches, ignore_index=True)
+
+phylum_counts = tRNA_downsampled_df['Phylum'].value_counts()  
+phylum_counts = phylum_counts.reindex(top_phyla.index) # In same order as top_phyla
+
+# matches in each phylum
+matches_phylum_counts = tRNA_downsampled_df[tRNA_downsampled_df['Match_status'] == 'Match'].groupby('Phylum').size()
+matches_phylum_counts = matches_phylum_counts.reindex(top_phyla.index).fillna(0).astype(int)
+
+
+# HISTOGRAM
+g = sns.FacetGrid(tRNA_downsampled_df, col="Phylum", col_order=top_phyla.index, sharey=False, col_wrap=3, height=4, aspect=1.2)
+g.map_dataframe(sns.histplot, x="tRNA_score", hue = "Macth_status", hue_order=["No_match", "Match"], multiple="stack", bins=bin_edges)
+g.set_axis_labels("tRNA score", "Number of Bacteria")
+
+for ax, phylum in zip(g.axes.flat, phylum_counts.index):
+    ax.set_title(f"{phylum} (n={phylum_counts[phylum]}, m={matches_phylum_counts[phylum]})")
+        
+g.set(xlim=(min_value - 0.001, max_value + 0.001))
+    
+plt.subplots_adjust(top=0.85)
+
+ # Add title
+if "?" in gene_name:
+    gene_name = gene_name.replace("?", "/")
+    
+if matches == 1: # if matches exists
+    plt.figtext(0.5, 0.95, f"Gene name: {gene_name}", ha="center", fontsize=14)
+else:
+    plt.figtext(0.5, 0.95, f"Gene name: {gene_name} - NO MATCHES", ha="center", fontsize=14)     
+     
+plt.close(g.figure)

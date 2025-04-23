11d109795f3134b911801a328b0efef57200f0a8
Johanna Lundstrom <jolunds@saga.math.chalmers.se>
2025-04-23 19:52:46 +0200

    ny kod för trna genes och fixat boxplots

--- tRNA/2. tRNA_genes.py
+++ tRNA/2. tRNA_genes.py
@@ -11,7 +11,7 @@ from Bio import SeqIO
 
 # Count how many times each codon appears
 def count_codons(sequence):
-    sequence = re.sub(r'^ATGCRYWSKMBDHVN]', '', sequence.upper()) 
+    sequence = re.sub(r'^ATGCRYWSKMBDHVN]', '', sequence.upper()) # Includes other letters so the codons will be correct
     codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
     return Counter(codon for codon in codons) #if len(codon) == 3)
 
@@ -29,9 +29,22 @@ for record in SeqIO.parse(fasta_file, "fasta"):
         rows.append({"Gene_name": gene_name, "Codon": codon, "Count": count})
 
 gene_codons_df = pd.DataFrame(rows)
-print(gene_codons_df)
 
 # Save to csv-file
 output_file = '/storage/jolunds/REVISED/tRNA/codons_genes.csv'
 gene_codons_df.to_csv(output_file, index=False)
+
+
+# Remove codons with letters that are not ACGT
+file = '/storage/jolunds/REVISED/tRNA/codons_genes.csv'
+gene_codons_df = pd.read_csv(file)
+
+incorrect_codons = ['GGN', 'NGG', 'GGS', 'GYC', 'KGG', 'AAR', 'ATY', 'CGY',
+ 'TCK', 'ANC', 'ANA', 'NAA', 'GNT', 'CNT', 'NTC', 'CGN', 'NTG', 'NAC', 'AAK', 'CMG',
+ 'TTY', 'CTY', 'ACK', 'YTG', 'CTN']
+
+gene_codons_df = gene_codons_df[~gene_codons_df["Codon"].isin(incorrect_codons)]
+
+save_path = "/storage/jolunds/REVISED/tRNA/codons_genes_filtered.csv"
+gene_codons_df.to_csv(save_path)
 

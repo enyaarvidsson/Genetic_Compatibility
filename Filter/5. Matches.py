be42b784f8bb24ed9050f31432e29f1db19caf31
Johanna Lundstrom <jolunds@saga.math.chalmers.se>
2025-06-23 09:56:29 +0200

    Alla gener

--- Filter/5. Matches.py
+++ Filter/5. Matches.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import time
-
+from tqdm import tqdm
 
 start_time = time.time()
 # Get the gene IDs and bacteria IDs from the BLAST results
@@ -9,35 +9,41 @@ with open(f"/storage/jolunds/FINAL/BLAST/filtered_blast_results.tsv") as f: # op
 
 gene_header = [line.split("\t")[0] for line in lines]
 
-gene_name = [line.split("|")[5] for line in gene_header] # Takes gene names
+gene_names = [line.split("|")[5] for line in gene_header] # Takes gene names
 
 bacteria_id_seq = [line.split("\t")[1] for line in lines] # Takes the ID for the sequences in the bacterial genomes
 bacteria_id = [line.split("|")[0] for line in bacteria_id_seq] # Removes sequence ID, so we just have bacteria IDs
 
 blast_results_df = pd.DataFrame({
-        "Gene_name": gene_name,
+        "Gene_name": gene_names,
         "Bacteria_ID": bacteria_id
     })
 
-unique_genes = blast_results_df["Gene_name"].unique()
+# Load gene names
+with open("/storage/enyaa/FINAL/gene_names.txt", "r") as f:
+    all_genes = [line.strip() for line in f]
 
 # Read taxonomy file
 taxonomy_file = "/storage/enyaa/FINAL/filtered_bacteria.csv"
 taxonomy_df = pd.read_csv(taxonomy_file) 
 
 gene_count = 0
-for gene in unique_genes:
+for gene_name in tqdm(all_genes, desc="Processing genes"):
     gene_count += 1
-    matched_bacteria = blast_results_df[blast_results_df["Gene_name"] == gene]["Bacteria_ID"]
-    gene_df = taxonomy_df.copy()
 
+    if gene_name in blast_results_df["Gene_name"].values:
+        matched_bacteria = blast_results_df[blast_results_df["Gene_name"] == gene_name]["Bacteria_ID"]
+    else:
+        matched_bacteria = []
+    
+    gene_df = taxonomy_df.copy()
     gene_df["Match_info"] = gene_df["Bacteria_ID"].apply(
         lambda x: "Match" if x in set(matched_bacteria) else "Non-match"
     )
 
-    if "/" in gene: # Just for look-up
-        gene = gene.replace("/", "?")
-    gene_df.to_csv(f"/storage/jolunds/FINAL/MATCH_INFO/{gene}_match_info.tsv", sep="\t", index=False)
+    if "/" in gene_name: # Just for look-up
+        gene_name = gene_name.replace("/", "?")
+    gene_df.to_csv(f"/storage/jolunds/FINAL/MATCH_INFO/{gene_name}_match_info.tsv", sep="\t", index=False)
 
 end_time = time.time()
 total_time = (end_time -start_time)/60

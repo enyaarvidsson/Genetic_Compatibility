a1df6b67e13a90e53ba43147254617948e983205
Enya Arvidsson <enyaa@saga.math.chalmers.se>
2025-07-03 15:57:40 +0200

    fix

--- tRNA_score/4. tRNA histogram.py
+++ tRNA_score/4. tRNA histogram.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 
 
-gene_name = "tet(W)" 
+gene_name = "tet(Q)" 
 
 
 np.random.seed(42)
@@ -15,15 +15,15 @@ np.random.seed(42)
 if "/" in gene_name:
     gene_name = gene_name.replace("/", "?")
 
-file_path = f"/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_{gene_name}.csv"  
+file_path = f"/storage/jolunds/FINAL/tRNA_SCORE/{gene_name}_tRNA_score.csv"
 tRNA_score_df = pd.read_csv(file_path)
-# denna borde innehålla match_status
+    # 67098 rows (bacteria), with columns: Bacteria_ID, tRNA_score, Worst_case_tRNA, Worst_codon, taxonomic info (multiple columns), Match_status
 
 matches = 1    
 no_match_count = tRNA_score_df["Match_status"].value_counts().get("Match", 0)    
 if no_match_count == 0:
-        print("No matches for gene:", gene_name)
-        matches = 0
+    print("No matches for gene:", gene_name)
+    matches = 0
 
 # Filter for top phyla
 top_phyla = tRNA_score_df["Phylum"].value_counts().head(6)
@@ -35,7 +35,7 @@ top_phyla = top_phyla.loc[phyla_order]
 tRNA_score_df = tRNA_score_df[tRNA_score_df['Phylum'].isin(top_phyla.index)]
 
 
-# DOWNSAMPLE NO_MATCH
+# DOWNSAMPLE NON-MATCH
 downsampled_no_matches = [] # will become a list of dataframes
 tRNA_score_df = tRNA_score_df.copy()
 tRNA_score_df["Phylum"] = tRNA_score_df["Phylum"].astype(str)
@@ -44,29 +44,27 @@ for phylum, phylum_df in tRNA_score_df.groupby("Phylum"): # phylum - name of phy
     match_count = (phylum_df["Match_status"] == "Match").sum()
     # create a df for match and a df for no_match
     matches_phylum_df = phylum_df[phylum_df["Match_status"] == "Match"]
-    no_matches_phylum_df = phylum_df[phylum_df["Match_status"] == "No_match"]  
+    no_matches_phylum_df = phylum_df[phylum_df["Match_status"] == "Non-match"]  
 
     # How many no_matches to keep
     if matches == 1: # if matches exists
         if match_count == 0:
             keep_size = 5000
         elif match_count < 100:
-            #keep_size = 2000
             keep_size = 1000
         elif match_count < 3000:
-            #keep_size = 10000
             keep_size = 5000
         else:
             keep_size = match_count * 3
     else:
         keep_size = len(phylum_df) 
 
-    # Downsample no_matches
+    # Downsample non-matches
     if keep_size > len(no_matches_phylum_df): # make sure np.random.choice works, so the keep_size is not bigger than the population size
         keep_size = len(no_matches_phylum_df)
     downsampled_no_matches_phylum_df = no_matches_phylum_df.iloc[np.random.choice(len(no_matches_phylum_df), keep_size, replace=False)]
 
-    # Append both "Match" and downsampled "No_match" bacteria
+    # Append both "Match" and downsampled "Non-match" bacteria
     downsampled_no_matches.append(pd.concat([matches_phylum_df, downsampled_no_matches_phylum_df]))
 
 # Combine all phyla into the final df
@@ -80,19 +78,19 @@ matches_phylum_counts = tRNA_downsampled_df[tRNA_downsampled_df['Match_status'] 
 matches_phylum_counts = matches_phylum_counts.reindex(top_phyla.index).fillna(0).astype(int)
 
 nr_bins = 30
-min_value = tRNA_downsampled_df["tRNA_score_one_sided"].min()
-max_value = tRNA_downsampled_df["tRNA_score_one_sided"].max() #+ 0.001 # so all values fall inside the max_value
+min_value = tRNA_downsampled_df["tRNA_score"].min()
+max_value = tRNA_downsampled_df["tRNA_score"].max() 
 bin_edges = np.linspace(min_value, max_value, nr_bins + 1)
 
 
 # HISTOGRAM
 g = sns.FacetGrid(tRNA_downsampled_df, col="Phylum", col_order=top_phyla.index, sharey=False, col_wrap=3, height=4, aspect=1.2)
-g.map_dataframe(sns.histplot, x="tRNA_score_one_sided", hue = "Match_status", hue_order=["No_match", "Match"], multiple="stack", bins=bin_edges)
+g.map_dataframe(sns.histplot, x="tRNA_score", hue = "Match_status", hue_order=["Non-match", "Match"], multiple="stack", bins=bin_edges)
 g.set_axis_labels("tRNA score", "") # Number of bacteria
 
 for ax, phylum in zip(g.axes.flat, phylum_counts.index):
-    #ax.set_title(f"{phylum} (n={phylum_counts[phylum]}, m={matches_phylum_counts[phylum]})", fontsize=15)
-    ax.set_title(f"{phylum}", fontsize=15)
+    ax.set_title(f"{phylum} (n={phylum_counts[phylum]}, m={matches_phylum_counts[phylum]})", fontsize=15)
+    #ax.set_title(f"{phylum}", fontsize=15)
     ax.set_xlabel("tRNA score", fontsize=15)
     ax.tick_params(axis='both', labelsize=13)    
 
@@ -104,16 +102,13 @@ plt.subplots_adjust(top=0.85)
 if "?" in gene_name:
     gene_name = gene_name.replace("?", "/")
 
-tRNA_score_title = "one-sided"
-tRNA_score_nr = "1"
-
 if matches == 1: # if matches exists
-    plt.figtext(0.5, 0.95, f"Gene name: {gene_name} {tRNA_score_title}", ha="center", fontsize=14)
+    plt.figtext(0.5, 0.95, f"Gene name: {gene_name}", ha="center", fontsize=14)
 else:
-    plt.figtext(0.5, 0.95, f"Gene name: {gene_name} {tRNA_score_title} - NO MATCHES", ha="center", fontsize=14)     
+    plt.figtext(0.5, 0.95, f"Gene name: {gene_name} - NO MATCHES", ha="center", fontsize=14)     
 
-plt.tight_layout()
-plt.savefig(f'/home/enyaa/gene_genome/histogram{tRNA_score_nr}_{gene_name}.png')     
+#plt.tight_layout() # in report
+plt.savefig(f'/home/enyaa/gene_genome/histogram_tRNA_score_{gene_name}.png')     
 plt.close(g.figure)
 
-print(f"Histogram created for {gene_name} {tRNA_score_title}")
+print(f"tRNA score histogram created for {gene_name}")
--- tRNA_score/7. GC_content.py
+++ tRNA_score/7. GC_content.py
@@ -1,3 +1,7 @@
+# This script includes three parts:
+    # Calculating the GC-ratio and making a scatterplot (GC-ratio vs tRNA score)
+    # Calculating the GC-difference and making a scatterplot (GC-difference vs tRNA score)
+    # For the GC-ratio plot, dividing it up into bins and making all bins have the same number of datapoints
 
 import time
 import pandas as pd
@@ -12,8 +16,7 @@ import matplotlib.pyplot as plt
 # Calculates the GC-ratio between genes and genomes - only for filtered matches
 # Makes a scatterplot gc-ratio vs tRNA score
 
-
-start_time = time.time()
+""" start_time = time.time()
 
 # Load the files with GC-content for genes and genomes 
 file_bacteria = "/storage/enyaa/FINAL/GC/gc_content_bacteria.pkl"
@@ -21,7 +24,6 @@ file_genes = "/storage/enyaa/FINAL/GC/gc_content_genes.pkl"
 
 with open(file_bacteria, "rb") as f:
     bacteria_gc_df = pickle.load(f)
-    # blir detta rätt?
 with open(file_genes, "rb") as f:
     genes_gc_df = pickle.load(f)
 
@@ -42,8 +44,7 @@ for gene_name in tqdm(gene_names_df["Gene_name"], desc="Processing genes"):
     if "/" in gene_name:
         gene_name = gene_name.replace("/", "?")
 
-    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_{gene_name}.csv") 
-        # 70911 rows - should only contain 67089 rows?
+    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/FINAL/tRNA_SCORE/{gene_name}_tRNA_score.csv") 
 
     # filter to only include matching bacteria
     filtered_tRNA_gene_df = tRNA_gene_df[tRNA_gene_df["Match_status"] == "Match"]
@@ -60,7 +61,7 @@ for gene_name in tqdm(gene_names_df["Gene_name"], desc="Processing genes"):
     # Compute GC ratio
     ratio = np.round(gene_gc / bacteria_gc, 4) # array of the ratio for one gene vs all genomes
 
-    tRNA_score_all.extend(filtered_tRNA_gene_df["tRNA_score_one_sided"].values) 
+    tRNA_score_all.extend(filtered_tRNA_gene_df["tRNA_score"].values) 
     gc_ratio_all.extend(ratio)
 
 df_plot = pd.DataFrame({
@@ -75,64 +76,35 @@ plt.xlabel("tRNA score", fontsize=14)
 plt.ylabel("GC-ratio", fontsize=14)
 plt.tick_params(axis='both', labelsize=12)
 
-
-tRNA_score_title = "one-sided"
-tRNA_score_nr = "1"
-
-#plt.title(f"GC-ratio vs tRNA score ({tRNA_score_title}) for matching genes and genomes")
+#plt.title(f"GC-ratio vs tRNA score for matching genes and genomes")
 plt.tight_layout()
 plt.grid(True)
-plt.savefig(f'/home/enyaa/gene_genome/GC_ratio_vs_tRNA{tRNA_score_nr}.png') 
+plt.savefig(f'/home/enyaa/gene_genome/GC_ratio_vs_tRNA.png') 
 plt.close()
-
 
 end_time = time.time()
 total_time = (end_time - start_time)/60
-print(f"Scatterplot ratio created in: {total_time} minutes!")
-
+print(f"Scatterplot ratio created in: {total_time} minutes!") """
 
 
 # SCATTERPLOT DIFFERENCE for matches -------------------------
-
-# GÖR SAMMA SOM OVAN OM DET FUNKAR
-
 # Calculates the GC-difference between genes and genomes - only for filtered matches
-# Makes a scatterplot gc-diff vs tRNA score
-
-
-tRNA_score = "tRNA_score_one_sided"
-
-
+# Makes a scatterplot gc-difference vs tRNA score
+""" 
 start_time = time.time()
 
-# Load one tRNA_score file to take out the filtered bacteria 
-filepath_tRNA = "/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_tet(Q).csv"
-df = pd.read_csv(filepath_tRNA) 
-
-# only top phyla
-top_phyla = df["Phylum"].value_counts().head(6)
-df = df[df["Phylum"].isin(top_phyla.index)]
-
-bacteria_ids = df['Bacteria_ID'].unique().tolist()
-    # 67 089 bacteria_ids
-
 # Load the files with GC-content for genes and genomes 
-file_bacteria = "/storage/enyaa/REVISED/GC/gc_content_bacteria.pkl"
-file_genes = "/storage/enyaa/REVISED/GC/gc_content_genes.pkl"
+file_bacteria = "/storage/enyaa/FINAL/GC/gc_content_bacteria.pkl"
+file_genes = "/storage/enyaa/FINAL/GC/gc_content_genes.pkl"
 
 with open(file_bacteria, "rb") as f:
     bacteria_gc_df = pickle.load(f)
 with open(file_genes, "rb") as f:
     genes_gc_df = pickle.load(f)
 
-bacteria_gc_filtered_df = bacteria_gc_df[bacteria_gc_df['Bacteria_ID'].isin(bacteria_ids)]
-    # 67089 rows - one column Bacteria_ID, one column GC_content
-
-# this makes code faster
-available_taxonomy_files = set(os.listdir("/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/"))
 
 # Go through each gene
-gene_names = "/storage/enyaa/REVISED/gene_names.txt"
+gene_names = "/storage/enyaa/FINAL/gene_names.txt"
 gene_names_df = pd.read_csv(gene_names, header=None, names=["Gene_name"])
 
 tRNA_score_all = []
@@ -141,116 +113,74 @@ gc_diff_all = []
 for gene_name in tqdm(gene_names_df["Gene_name"], desc="Processing genes"): 
 
     gene_gc = genes_gc_df.loc[genes_gc_df['Gene_name'] == gene_name, 'GC_content'].values[0]
-
-    # ONLY COMPUTE GC-DIFF BETWEEN GENES AND ITS MATCHES
+    
+    # ONLY COMPUTE GC-DIFFERENCE BETWEEN GENES AND ITS MATCHES
 
     if "/" in gene_name:
         gene_name = gene_name.replace("/", "?")
 
-    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_{gene_name}.csv") 
-    tRNA_gene_df = tRNA_gene_df[tRNA_gene_df['Bacteria_ID'].isin(bacteria_ids)]
-
-    filename = f"taxonomy_results_{gene_name}.csv"
-    if filename not in available_taxonomy_files:
-        #print(f"File not found: {filename}")
-        continue
-
-    taxonomy_path = f"/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/{filename}"
-    taxonomy_gene_df = pd.read_csv(taxonomy_path)
-    matching_df = taxonomy_gene_df['Bacteria_ID']
+    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/FINAL/tRNA_SCORE/{gene_name}_tRNA_score.csv") 
 
     # filter to only include matching bacteria
-    filtered_tRNA_gene_df = tRNA_gene_df.merge(matching_df, on='Bacteria_ID', how='inner')
+    filtered_tRNA_gene_df = tRNA_gene_df[tRNA_gene_df["Match_status"] == "Match"]
 
     if filtered_tRNA_gene_df.empty: 
-        #print("No matches for gene:", gene_name)
         continue
-
-    filtered_gc_df = bacteria_gc_filtered_df.merge(
+    
+    filtered_gc_df = bacteria_gc_df.merge(
         filtered_tRNA_gene_df[['Bacteria_ID']], on='Bacteria_ID', how='inner'
     )
+
     bacteria_gc = filtered_gc_df['GC_content'].to_numpy()
 
     # Compute GC difference
     diff = np.round(gene_gc - bacteria_gc, 4) # array of the difference for one gene vs all genomes
 
-    tRNA_score_all.extend(filtered_tRNA_gene_df[tRNA_score].values) 
-    gc_diff_all.extend(diff) 
-
+    tRNA_score_all.extend(filtered_tRNA_gene_df["tRNA_score"].values) 
+    gc_diff_all.extend(diff)
 
 df_plot = pd.DataFrame({
     'tRNA_score': tRNA_score_all,
-    'GC_difference': gc_diff_all
-    #'Phylum': phylum_all
+    'GC_diff': gc_diff_all
 })
 
 # Scatterplot:
 plt.figure(figsize=(8, 6))
-plt.scatter(data=df_plot, x='tRNA_score', y='GC_difference', alpha=1, s=10, color="darkorange")
+plt.scatter(data=df_plot, x='tRNA_score', y='GC_diff', alpha=1, s=10, color= "darkorange")
 plt.xlabel("tRNA score", fontsize=14)
 plt.ylabel("GC-difference", fontsize=14)
 plt.tick_params(axis='both', labelsize=12)
 
-if tRNA_score == "tRNA_score_one_sided":
-    tRNA_score_title = "one-sided"
-    tRNA_score_nr = "1"
-else:
-    tRNA_score_title = "two-sided"
-    tRNA_score_nr = "2"
-
-#plt.title(f"GC-difference vs tRNA score ({tRNA_score_title}) for matching genes and genomes")
-#plt.legend(title='Phylum', bbox_to_anchor=(1.05, 1), loc='upper left')
+#plt.title(f"GC-difference vs tRNA score for matching genes and genomes")
 plt.tight_layout()
 plt.grid(True)
-plt.savefig(f'/home/enyaa/gene_genome/GC_diff_vs_tRNA{tRNA_score_nr}.png') 
+plt.savefig(f'/home/enyaa/gene_genome/GC_diff_vs_tRNA.png') 
 plt.close()
-
 
 end_time = time.time()
 total_time = (end_time - start_time)/60
-print(f"Scatterplot difference created in: {total_time} minutes!")
+print(f"Scatterplot difference created in: {total_time} minutes!") """
 
 
 # SCATTERPLOT RATIO - BINS for matches -----------------------------
-
-# FIXA EFTER OVAN ÄR FIXAT
-
 # Calculates the GC-ratio between genes and genomes - only for filtered matches
+# Divides into bins
 # Makes a scatterplot gc-ratio vs tRNA score
-#'''
-
-tRNA_score = "tRNA_score_one_sided"
 
 
 start_time = time.time()
 
-# Load one tRNA_score file to take out the filtered bacteria 
-filepath_tRNA = "/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_tet(Q).csv"
-df = pd.read_csv(filepath_tRNA) 
-
-# only top phyla
-top_phyla = df["Phylum"].value_counts().head(6)
-df = df[df["Phylum"].isin(top_phyla.index)]
-bacteria_ids = df['Bacteria_ID'].unique().tolist()
-    # 67 089 bacteria_ids
-
 # Load the files with GC-content for genes and genomes 
-file_bacteria = "/storage/enyaa/REVISED/GC/gc_content_bacteria.pkl"
-file_genes = "/storage/enyaa/REVISED/GC/gc_content_genes.pkl"
+file_bacteria = "/storage/enyaa/FINAL/GC/gc_content_bacteria.pkl"
+file_genes = "/storage/enyaa/FINAL/GC/gc_content_genes.pkl"
 
 with open(file_bacteria, "rb") as f:
     bacteria_gc_df = pickle.load(f)
 with open(file_genes, "rb") as f:
     genes_gc_df = pickle.load(f)
 
-bacteria_gc_filtered_df = bacteria_gc_df[bacteria_gc_df['Bacteria_ID'].isin(bacteria_ids)]
-    # 67089 rows - one column Bacteria_ID, one column GC_content
-
-# this makes code faster
-available_taxonomy_files = set(os.listdir("/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/"))
-
 # Go through each gene
-gene_names = "/storage/enyaa/REVISED/gene_names.txt"
+gene_names = "/storage/enyaa/FINAL/gene_names.txt"
 gene_names_df = pd.read_csv(gene_names, header=None, names=["Gene_name"])
 
 tRNA_score_all = []
@@ -265,56 +195,33 @@ for gene_name in tqdm(gene_names_df["Gene_name"], desc="Processing genes"):
     if "/" in gene_name:
         gene_name = gene_name.replace("/", "?")
 
-    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/REVISED/tRNA/tRNA_score_new/tRNA_score_{gene_name}.csv") 
-        # 70911 rows 
-    tRNA_gene_df = tRNA_gene_df[tRNA_gene_df['Bacteria_ID'].isin(bacteria_ids)]
-        # 67089 rows
-
-    filename = f"taxonomy_results_{gene_name}.csv"
-    if filename not in available_taxonomy_files:
-        #print(f"File not found: {filename}")
-        continue
-
-    taxonomy_path = f"/storage/jolunds/REVISED/TAXONOMY/taxonomy_split_genes/{filename}"
-    taxonomy_gene_df = pd.read_csv(taxonomy_path)
-    matching_df = taxonomy_gene_df['Bacteria_ID'] #, 'Phylum']]
+    tRNA_gene_df = pd.read_csv(f"/storage/jolunds/FINAL/tRNA_SCORE/{gene_name}_tRNA_score.csv") 
 
     # filter to only include matching bacteria
-    filtered_tRNA_gene_df = tRNA_gene_df.merge(matching_df, on='Bacteria_ID', how='inner')
+    filtered_tRNA_gene_df = tRNA_gene_df[tRNA_gene_df["Match_status"] == "Match"]
 
     if filtered_tRNA_gene_df.empty: 
-        #print("No matches for gene:", gene_name)
         continue
     
-    filtered_gc_df = bacteria_gc_filtered_df.merge(
+    filtered_gc_df = bacteria_gc_df.merge(
         filtered_tRNA_gene_df[['Bacteria_ID']], on='Bacteria_ID', how='inner'
     )
-    if filtered_gc_df.empty:
-        print("denna ska ej behövas")
-        continue
-    bacteria_gc = filtered_gc_df['GC_content'].to_numpy()
 
-    #filtered_tRNA_gene_df = filtered_tRNA_gene_df.merge(
-    #    bacteria_gc_filtered_df[['Bacteria_ID', 'GC_content']], on='Bacteria_ID', how='inner'
-    #)
-    #if filtered_tRNA_gene_df.empty:
-    #    continue
-    #bacteria_gc = filtered_tRNA_gene_df['GC_content'].to_numpy()
+    bacteria_gc = filtered_gc_df['GC_content'].to_numpy()
 
     # Compute GC ratio
     ratio = np.round(gene_gc / bacteria_gc, 4) # array of the ratio for one gene vs all genomes
 
-    tRNA_score_all.extend(filtered_tRNA_gene_df[tRNA_score].values) 
+    tRNA_score_all.extend(filtered_tRNA_gene_df["tRNA_score"].values) 
     gc_ratio_all.extend(ratio)
 
 df_plot = pd.DataFrame({
     'tRNA_score': tRNA_score_all,
     'GC_ratio': gc_ratio_all
-    #'Phylum': phylum_all
 })
 
 
-# BINS -------------
+# BINS ---
 # Add a column bin to the df, with the labels
 bins = [0, 0.5, 0.75, 1.0, 1.25, 1.5, float('inf')]
 labels = ['<0.5', '0.5-0.75', '0.75-1.0', '1.0-1.25', '1.25-1.5', '>1.5']
@@ -336,15 +243,9 @@ df_target['bin'] = df_target['bin'].cat.remove_unused_categories()
 bin_counts = df_target['bin'].value_counts()
 print(bin_counts)
 
-# Get the smallest count (for bins that actually have data)
+# Get the smallest count
 min_count = bin_counts.min()
 print(min_count)
-
-# Sample min_count from each of the target bins
-#sampled_dfs = [
-#    df_target[df_target['bin'] == b].sample(n=min_count, random_state=1)
-#    for b in target_bins if (df_target['bin'] == b).sum() >= min_count
-#]
 
 # Group by bin and sample min_count rows from each group
 sampled_dfs = (
@@ -354,7 +255,7 @@ sampled_dfs = (
     .reset_index(drop=True)
 )
 
-# Keep all rows from <0.5 and >1.5 (unchanged bins)
+# Keep all rows from <0.5 and >1.5 
 other_bins_df = df_plot[df_plot['bin'].isin(['<0.5', '>1.5'])]
 
 # Concatenate the result
@@ -369,22 +270,16 @@ plt.xlabel("tRNA score", fontsize=14)
 plt.ylabel("GC-ratio", fontsize=14)
 plt.tick_params(axis='both', labelsize=12)
 
-if tRNA_score == "tRNA_score_one_sided":
-    tRNA_score_title = "one-sided"
-    tRNA_score_nr = "1"
-else:
-    tRNA_score_title = "two-sided"
-    tRNA_score_nr = "2"
 
-#plt.title(f"GC-ratio vs tRNA score ({tRNA_score_title}) for matching genes and genomes")
+#plt.title(f"GC-ratio vs tRNA score for matching genes and genomes")
 #plt.legend(title='Phylum', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
-plt.savefig(f'/home/enyaa/gene_genome/bins2.png') 
+plt.savefig(f'/home/enyaa/gene_genome/bins.png') 
 plt.close()
 
 
 end_time = time.time()
 total_time = (end_time - start_time)/60
-print(f"Scatterplot ratio created in: {total_time} minutes!")
+print(f"Scatterplot ratio bins created in: {total_time} minutes!")
 
